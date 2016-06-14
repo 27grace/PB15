6a189eeff4b8caf886194326e7dc0a401decace6
fanmo04 <michaelamofan@yahoo.co.uk>
2016-06-14 22:00:04 +0100

    update

--- retracing_steps.py
+++ retracing_steps.py
@@ -1,3 +1,7 @@
+import pyb
+from pyb import *
+print('Bluetooth testing')
+
 print('Initialising retracing_steps')
 
 from pyb import Pin, ADC, Timer
@@ -36,11 +40,11 @@ def drive(speed): # Set direction to forward
     
 def rightturn(): #R turn
 	# stop
-	while speed > 0:
-		stop() # stop
-
+	stop() 
+	pyb.delay(500)
+	
 	# Reset motor direction to turn
-    A1.high()
+	A1.high()
 	A2.low()
 	B1.high()
 	B2.low()
@@ -54,12 +58,11 @@ def rightturn(): #R turn
 	pyb.delay(500) # pause before continuing
     
 def leftturn(): #L turn
-	# stop
-	while speed > 0:
-		stop() # stop
-
+	stop() # stop
+	pyb.delay(500)
+	
 	# Reset motor direction to turn
-    A1.low()
+	A1.low()
 	A2.high()
 	B1.low()
 	B2.high()
@@ -74,19 +77,20 @@ def leftturn(): #L turn
 
 def backward():
 	# stop
-	while speed > 0:
-		stop() # stop
+	stop() # stop
+	pyb.delay(500)
 
     # Reverse motor direction
-    A1.high()
+	A1.high()
 	A2.low()
 	B1.low()
 	B2.high()
     
    	stop() #stop
 	pyb.delay(500) # pause before continuing
+	ch1.pulse_width_percent(speed) # send a pulse of width 'speed'% to motor A
+	ch2.pulse_width_percent(speed)
 
-# Get Keypad commands via Bluetooth (based from Task 8)-----------------------------------
 key = ('1', '2', '3', '4', 'U', 'D', 'L', 'R') 
 uart = UART(6)
 uart.init(9600, bits=8, parity=None, stop=2) 
@@ -95,32 +99,40 @@ while True:
 		n=uart.any()   
 	command = uart.read(10) #reading the ASCII code for when a button is pressed
 	key_index = command[2]-ord('1') 
+	action = 'none'
 	if command[3]==ord('1'):
   		test = 'something is pressed'
+  		print (test)
 	#if 1 is pressed
-  		elif (key_index==0):
-  			action = '1 pressed'
+		if (key_index == 0):
+			action = '1 pressed'
+			stop()
   	#if 2 is pressed
-  		elif (key_index==1):
-  			action = '2 pressed'
+		elif (key_index==1):
+			action = '2 pressed'
   	#if 3 is pressed
-  		elif (key_index==2):
- 	 		action = '3 pressed'
+		elif (key_index==2):
+ 			action = '3 pressed'
   	#if 4 is pressed
-  		elif (key_index==3):
- 	 		action = '4 pressed'
+		elif (key_index==3):
+ 			action = '4 pressed'
   	#if U is pressed
-  		elif (key_index==4):
-  			action = 'UP pressed'
+		elif (key_index==4):
+			action = 'UP pressed'
+			drive(speed)
   	#if D is pressed
-  		elif (key_index==5):
-  			action = 'DOWN pressed'
+		elif (key_index==5):
+			action = 'DOWN pressed'
+			backward()
   	#if L is pressed
-  		elif (key_index==6):
- 	 		action = 'LEFT pressed'
+		elif (key_index==6):
+			action = 'LEFT pressed'
+			leftturn()
   	#if R is pressed
-  		elif (key_index==7):
- 	 		action = 'RIGHT pressed'
+		elif (key_index==7):
+			action = 'RIGHT pressed'
+			rightturn()
 	else: 
-	  	action = 'nothing pressed'
-print ('Key ', test, '', action)
+		action = 'nothing pressed'
+	print ('Key ', test, '', action)
+	
