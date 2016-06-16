55884287e65267e0891e90db181c3cc41c81fa4d
27grace <27grace@gmail.com>
2016-06-16 17:04:28 +0100

    u

--- ms2_retracing_steps_updated.py
+++ ms2_retracing_steps_updated.py
@@ -83,7 +83,6 @@ def backward():
 	# stop
 	stop() # stop
 	pyb.delay(500)
-
     # Reverse motor direction
 	A1.high()
 	A2.low()
@@ -98,22 +97,22 @@ def reset_memory():
 	memory[:] = []
 	
 def run_memory():
-	#recording final time..
+     #recording final time..
 	time = pyb.elapsed_millis(start) 
   	times.append(time)
-  	print (time) #checking
-	#creating inverse_memory
+  	print (time) #checking	
+  	del times[0] 
+     #creating inverse_memory
 	inverse_memory=[]
 	for i in range(1, len(memory)+1):
 		inverse_memory.append(memory[-i])
 	print (inverse_memory)
-	#creating inverse_times
+     #creating inverse_times
 	inverse_times=[]
 	for i in range(1, len(times)+1):
-		inverse_memory.append(times[-i])
+		inverse_times.append(times[-i])
 	print (inverse_times)
-	del times[0] 
-	#retracing steps...
+     #retracing steps...
 	for i in range(0, len(inverse_memory)-1):
 		inverse_memory[i]()
 		pyb.delay(inverse_times[i])
@@ -132,6 +131,7 @@ while True:
 	command = uart.read(10) #reading the ASCII code for when a button is pressed
 	key_index = command[2]-ord('1') 
 	action = 'none'
+	#something is pressed
 	if command[3]==ord('1'):
   		test = 'something is pressed'
   		print (test)
@@ -152,7 +152,6 @@ while True:
 		elif (key_index==2):
  			action = '3 pressed'
 			run_memory()
-
   	#if 4 is pressed
 		elif (key_index==3):
  			action = '4 pressed'
@@ -161,19 +160,16 @@ while True:
 			action = 'UP pressed'
 			drive()
 			memory.append(backward)
-			
   	#if D is pressed
 		if (key_index==5):
 			action = 'DOWN pressed'
 			backward()
 			memory.append(drive)
-			
   	#if L is pressed
 		if (key_index==6):
 			action = 'LEFT pressed'
 			leftturn()
 			memory.append(rightturn)
-		
   	#if R is pressed
 		if (key_index==7):
 			action = 'RIGHT pressed'
