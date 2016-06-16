10f28d840ea4ac54a82caadae3254cdea1fc2373
27grace <27grace@gmail.com>
2016-06-16 17:13:54 +0100

    servo updated

--- ms2+3_taking_penalty.py
+++ ms2+3_taking_penalty.py
@@ -24,12 +24,174 @@ ch2 = tim.channel(2, Timer.PWM, pin = motor2)
 
 servo = Servo(3) # servo on position 3 (X3, VIN, GND)
 
-# Defining servo function (To be placed under 'Defining Button Functions') --------------------------
+
+# Defining button functions--------------------------------------------------
+def stop():
+	ch1.pulse_width_percent(0) # send a pulse of width 0% to motor A
+	ch2.pulse_width_percent(0) # send a pulse of width 0% to motor B
+
+def drive(): # Set direction to forward
+	A1.low() # Motor A set forward
+	A2.high()
+
+	B1.high() # Motor B set forward
+	B2.low()
+
+	ch1.pulse_width_percent(speed) # send a pulse of width 'speed'% to motor A
+	ch2.pulse_width_percent(speed) # send a pulse of width 'speed'% to motor B
+    
+def rightturn(): #R turn
+	# stop
+	stop() 
+	pyb.delay(500)
+	
+	# Reset motor direction to turn
+	A1.low()
+	A2.high()
+	B1.low()
+	B2.high()
+
+	# turn on the spot
+	ch1.pulse_width_percent(40)
+	ch2.pulse_width_percent(40)
+	pyb.delay(500) # delay to allow the turn
+
+	stop() #stop
+	pyb.delay(500) # pause before continuing
+    
+def leftturn(): #L turn
+	stop() # stop
+	pyb.delay(500)
+	
+	# Reset motor direction to turn
+	A1.high()
+	A2.low()
+	B1.high()
+	B2.low()
+
+	# turn on the spot
+	ch1.pulse_width_percent(40)
+	ch2.pulse_width_percent(40)
+	pyb.delay(500) # delay to allow the turn
+
+	stop() #stop
+	pyb.delay(500) # pause before continuing
+
+def backward():
+	# stop
+	stop() # stop
+	pyb.delay(500)
+
+    # Reverse motor direction
+	A1.high()
+	A2.low()
+	B1.low()
+	B2.high()
+    
+	ch1.pulse_width_percent(speed) # send a pulse of width 'speed'% to motor A
+	ch2.pulse_width_percent(speed)
+
+def reset_memory():
+	print (memory)
+	memory[:] = []
+	
+def run_memory():
+  	del times[0] 
+	#creating inverse_memory
+	inverse_memory=[]
+	for i in range(1, len(memory)+1):
+		inverse_memory.append(memory[-i])
+	print (inverse_memory)
+	#creating inverse_times
+	inverse_times=[]
+	for i in range(1, len(times)+1):
+		inverse_times.append(times[-i])
+	print (times)
+	print (len(times))
+	print (inverse_times)
+	print (len(inverse_memory))
+	print (len(inverse_times))
+	#retracing steps...
+	for i in range(0, len(inverse_memory)):
+		inverse_memory[i]()
+		pyb.delay(inverse_times[i])
+
 def servoswing():
     servo.angle(90) # move to the release position
     pyb.delay(500)
     servo.angle(-45, 1000) # move to ready position in 1000 milisecs
     pyb.delay(500)
     
+#--------------------------------------------------------------------
+		
+key = ('1', '2', '3', '4', 'U', 'D', 'L', 'R') 
+uart = UART(6)
+uart.init(9600, bits=8, parity=None, stop=2) 
 
+memory =[]
+times = []
+start = 0
+
+servo.angle(-45, 1000) # move to ready position in 1000 milisecs
+
+while True: 
+	while (uart.any()!=10): #wait we get 10 chars
+		n=uart.any()   
+	command = uart.read(10) #reading the ASCII code for when a button is pressed
+	key_index = command[2]-ord('1') 
+	action = 'none'
+	if command[3]==ord('1'):
+  		test = 'something is pressed'
+  		print (test)
+  		time = pyb.elapsed_millis(start) 
+  		times.append(time)
+  		print (time) #checking 
+  		start = pyb.millis()
+	#if 1 is pressed
+		if (key_index == 0):
+			action = '1 pressed'
+			stop()
+			memory.append(stop)
+  	#if 2 is pressed
+		elif (key_index==1):
+			action = '2 pressed'
+			reset_memory()
+  	#if 3 is pressed
+		elif (key_index==2):
+ 			action = '3 pressed'
+			run_memory()
+
+  	#if 4 is pressed
+		elif (key_index==3):
+ 			action = '4 pressed'
+ 			servoswing()
+ 			
+  	#if U is pressed
+		if (key_index==4):
+			action = 'UP pressed'
+			drive()
+			memory.append(backward)
+			
+  	#if D is pressed
+		if (key_index==5):
+			action = 'DOWN pressed'
+			backward()
+			memory.append(drive)
+			
+  	#if L is pressed
+		if (key_index==6):
+			action = 'LEFT pressed'
+			leftturn()
+			memory.append(rightturn)
+		
+  	#if R is pressed
+		if (key_index==7):
+			action = 'RIGHT pressed'
+			rightturn()
+			memory.append(leftturn)
+
+	else: 
+		action = 'nothing pressed'
+	print ('Key ', test, '', action)
+	
 
